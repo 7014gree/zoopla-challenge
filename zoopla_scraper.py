fa2d44d888d854498fd4f1f9ac280154f0b4a2ca
7014gree <7014gree@gmail.com>
2022-11-08 01:25:08 +0000

    Refactored to make ZooplaScraper class which uses class methods to do the scraping. Refactored how listing_dict is populated. Made small changes to output txt files and print statements. Added if __name__ = __main__.

--- zoopla_scraper.py
+++ zoopla_scraper.py
@@ -5,54 +5,78 @@ from bs4 import BeautifulSoup
 def write_to_txt(text, path):
     with open(path, 'w', encoding="utf-8") as f:
             f.write(text)
-    print("Written successfully")
+    print(f"Written to {path} successfully.")
 
+class ZooplaScraper:
+    def __init__(self, url):
+        self.url = url
 
-property_information = []
-next_page_url = "https://www.zoopla.co.uk/new-homes/property/london/"
+    def request_html(self):
+        r = requests.get(self.url)
+        html_string = r.text
+        soup = BeautifulSoup(html_string, 'html.parser')
+        return soup
+    
+    def set_next_url(self, soup):
+        next_page_button_tag = soup.find(name = 'li', attrs={'class': "css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"})
+        self.url = "https://www.zoopla.co.uk" + next_page_button_tag.find(name = 'a', attrs={'class': "eaoxhri5 css-xtzp5a-ButtonLink-Button-StyledPaginationLink eaqu47p1"})['href']
 
-for page in range(1, 6):
-    r = requests.get(next_page_url)
-    print(page, next_page_url)
-    html_string = r.text
-    soup = BeautifulSoup(html_string, 'html.parser')
+    def get_house_listings(self, soup):
+        listings_on_page = soup.find_all(name = 'div', attrs={'class': "c-iyCMIE c-iyCMIE-rwbZU-borderBottom-true"})
+        property_information = []
+        for listing in listings_on_page:
+            property_information.append(self.listing_to_dict(listing))
+        return property_information
 
-    s = soup.find_all(name = 'div', attrs={'class': "c-iyCMIE c-iyCMIE-rwbZU-borderBottom-true"})
+    def listing_to_dict(self, listing):
+        listing_url =  "https://www.zoopla.co.uk" + listing.find(name = 'a', attrs={'class': "c-enSgsc"})['href']
 
-    for listing in s:
-        listing_dict = {'listing url': "", 'summary': "", 'price': 0, 'bedrooms': 0, 'bathrooms': 0, 'living rooms': 0, 'address': "",
-    'nearest station 1': "", 'nearest station 2': "", 'other info': [], 'date listed': ""}
+        summary =  listing.find(name = 'h2', attrs={'data-testid': "listing-title"}).text
 
-        listing_dict['listing url'] = "https://www.zoopla.co.uk" + listing.find(name = 'a', attrs={'class': "c-enSgsc"})['href']
-
-        listing_dict['summary'] = listing.find(name = 'h2', attrs={'data-testid': "listing-title"}).text
-
-        listing_dict['price'] = listing.find(name = 'p', attrs={'class': "c-bTssUX", 'data-testid': "listing-price"}).text
+        price = int(listing.find(name = 'p', attrs={'class': "c-bTssUX", 'data-testid': "listing-price"}).text.replace(",","").replace("£",""))
         
         room_info = listing.findAll(name = 'span', attrs={'class': "c-PJLV"})
 
-        listing_dict['bedrooms'] = room_info[0].text
-        listing_dict['bathrooms'] = room_info[0].text
-        listing_dict['living rooms'] = room_info[0].text
+        bedrooms = room_info[0].text
+        bathrooms = room_info[0].text
+        living_rooms = room_info[0].text
 
-        listing_dict['address'] = listing.find(name = 'h3', attrs={'class': "c-eFZDwI"}).text
+        address = listing.find(name = 'h3', attrs={'class': "c-eFZDwI"}).text
 
         station_info = listing.findAll(name = 'span', attrs={'class': "c-UazGY"})
-        listing_dict['nearest station 1'] = station_info[0].text
-        listing_dict['nearest station 2'] = station_info[1].text
+        nearest_station_1 = station_info[0].text
+        nearest_station_2 = station_info[1].text
 
-        other_info = listing.findAll(name = 'div', attrs={'class': "c-PJLV c-PJLV-kQvhQW-centered-true c-PJLV-iPJLV-css"})
-        for html_tag in other_info:
-            listing_dict['other info'].append(html_tag.text)
+        other_info_html = listing.findAll(name = 'div', attrs={'class': "c-PJLV c-PJLV-kQvhQW-centered-true c-PJLV-iPJLV-css"})
+        other_info = []
+        for html_tag in other_info_html:
+            other_info.append(html_tag.text)
+
+        date_listed = listing.find(name = 'li', attrs={'class': "c-eUGvCx"}).text
+
+        listing_dict = {
+            'listing url': listing_url,
+            'summary': summary,
+            'price': price,
+            'bedrooms': bedrooms,
+            'bathrooms': bathrooms,
+            'living rooms': living_rooms,
+            'address': address,
+            'nearest station 1': nearest_station_1,
+            'nearest station 2': nearest_station_2,
+            'other info': other_info,
+            'date listed': date_listed
+            }
+
+        return listing_dict
 
 
-        listing_dict['date listed'] = listing.find(name = 'li', attrs={'class': "c-eUGvCx"}).text
+if __name__ == "__main__":
+    z = ZooplaScraper("https://www.zoopla.co.uk/new-homes/property/london/")
+    for i in range(5):
+        soup = z.request_html()
+        house_listings = z.get_house_listings(soup)
+        print(f"Scraping: {z.url}")
+        write_to_txt(f"{z.url}\n\n{house_listings}",f"text-{i + 1}.txt")
+        z.set_next_url(soup)
 
-        property_information.append(listing_dict)
-
-    write_to_txt(f"{next_page_url}\n\n{property_information}", f"text-{page}.txt")
-
-    next_page_tag = soup.find(name = 'li', attrs={'class': "css-qhg1xn-PaginationItemPreviousAndNext-PaginationItemNext eaoxhri2"})
-    next_page_url = "https://www.zoopla.co.uk" + next_page_tag.find(name = 'a', attrs={'class': "eaoxhri5 css-xtzp5a-ButtonLink-Button-StyledPaginationLink eaqu47p1"})['href']
-
-    
